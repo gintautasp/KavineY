26444eba6ed313f244e11061b399d91855a5d0d7
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2019-09-11 15:50:51 +0300

    bandomasis pakeitimas su crud paveldejimu

--- src/main/resources/static/crud.js
+++ src/main/resources/static/crud.js
@@ -4,55 +4,52 @@
 	*
 	*param field_data jei duomenu masyvas giliau tai lauko pavadiniamas, su tasku pradzioje, jei dar giliau - lauku seka, su taskais, bet butinai su tasku pradzioje, kitu atveju tuscias stringas
 	*/
-
 	function crud ( 
 		
-		fields
-		, fields_names
-		, url
-		, field_data
-		, fields_ids
-		, id_html_saraso
-		, id_html_dialog_formos
-		, dialog_size_x
-		, dialog_size_y
-		, title_dialog_form
-		, url_save_rec  
-		, url_delete_rec
-		, fields_edit
-		, field_name
+		params
 	) {
+		this.params = {};
+		this.params.fields = params.fields;
+		this.params.fields_names = params.fields_names;
+		this.params.url = params.url;
+		this.params.field_data = params.field_data;
+		this.params.fields_ids = params.fields_ids;
+		this.params.id_html_saraso = params.id_html_saraso;
+		this.params.id_html_dialog_formos = params.id_html_dialog_formos;
+		this.params.dialog_size_x = params.dialog_size_x;
+		this.params.dialog_size_y = params.dialog_size_y;
+		this.params.title_dialog_form = params.title_dialog_form;
+		this.params.url_save_rec = params.url_save_rec;
+		this.params.url_delete_rec = params.url_delete_rec;
 		
-		this.fields = fields;
-		this.fields_names = fields_names;
-		this.field_data = field_data;
-		this.fields_ids =fields_ids;
-		this.id_html_saraso = id_html_saraso;
-		this.id_html_dialog_formos = id_html_dialog_formos;
-		this.dialog_size_x = dialog_size_x;
-		this.dialog_size_y = dialog_size_y;
-		this.title_dialog_form =title_dialog_form;
-		this.url_save_rec = url_save_rec;
+		this.params.ed_fields = params.fields_edit;
 		
-		this.dialog = null;
-		this.form = null;
-		this.ed_fields = fields_edit;
-		this.o_ed_fields = [];
-		this.field_name = field_name;
+		this.params.field_name = params.field_name;
 		
-		this.allFields = $( [] );
+		var i_am = this;		
 		
-		this.data = {};
-																																	// alert ( 'url: ' + url );
-		this.res_str = '';
+		this.initEmpty = function() {
 		
-		var i_am = this;
+			this.params.o_ed_fields = [];
+
+			this.params.dialog = null;
+			this.params.form = null;		
+			
+			
+			this.params.allFields = $( [] );
+			
+			this.params.data = {};
+																																		// alert ( 'url: ' + url );
+			this.params.res_str = '';
+		}
+
+	//	this.initEmpty();
 		
 		this.refreshData = function() {
 		
 			$.ajax(
 					{
-						url: url
+						url: this.params.url
 														
 					}
 				)
@@ -60,53 +57,53 @@
 					
 					function( data ) {
 
-						i_am.data = data;
-																																		// alert ( 'this.data 1 ' +JSON.stringify ( i_am.data  ) );
+						i_am.params.data = data;
+																																		// alert ( 'this.data 1 ' +JSON.stringify ( i_am.params.data  ) );
 						i_am.sarasas();
 					}
 				);
 		}
 		
-		this.refreshData();		
+	//	this.refreshData();		
 
 		this.sarasas = function() {
 			
-			this.res_str = '<table>';			
+			this.params.res_str = '<table>';			
 			
 			this.sarasoAntraste();
-																														// alert ( 'this.data 2 ' + JSON.stringify ( this.data ) );	
-			num_produktu = eval ( 'this.data' + this.field_data + '.length' );
-																															// alert ( 'num_produktu:' +  num_produktu );
+																														alert ( 'this.params.data 2 ' + JSON.stringify ( this.params.data ) );	
+			num_produktu = eval ( 'this.params.data' + this.params.field_data + '.length' );
+																														alert ( 'num_produktu:' +  num_produktu );
 			for ( i = 0; i < num_produktu; i++) {
 																																					// alert ( i );
-				this.res_str += '<tr data-id="' + eval ( 'this.data' +this.field_data + '[ i ].id' ) +'" ><td>' + i + '</td>';
-																															// alert ( 'this.fields.length' + this.fields.length );
-				for ( k=0; k < this.fields.length; k++ ) {
+				this.params.res_str += '<tr data-id="' + eval ( 'this.params.data' +this.params.field_data + '[ i ].id' ) +'" ><td>' + i + '</td>';
+																															// alert ( 'this.params.fields.length' + this.params.fields.length );
+				for ( k=0; k < this.params.fields.length; k++ ) {
 				
-					this.res_str += '<td>'+ eval (  'this.data' + this.field_data +'[ i ].' + this.fields [ k ] ) + '</td>';
+					this.params.res_str += '<td>'+ eval (  'this.params.data' + this.params.field_data +'[ i ].' + this.params.fields [ k ] ) + '</td>';
 				}
 				
-				 this.res_str += '<td><input type="button" class="edit_button" value="redaguoti"><input  type="button" class="delete_button" value="šalinti"></td></tr>';
+				 this.params.res_str += '<td><input type="button" class="edit_button" value="redaguoti"><input  type="button" class="delete_button" value="šalinti"></td></tr>';
 			} 
-			this.res_str += '</table>';
+			this.params.res_str += '</table>';
 			
-			if ( this.id_html_dialog_formos == '' ) {
+			if ( this.params.id_html_dialog_formos == '' ) {
 
 				this.htmlDialogo();
-				this.id_html_dialog_formos = 'dialogo_forma';
+				this.params.id_html_dialog_formos = 'dialogo_forma';
 			}
 
-			$( '#' + this.id_html_saraso ).html( this.res_str );
+			$( '#' + this.params.id_html_saraso ).html( this.params.res_str );
 			this.initDialog();
 			this.edFields();
 			
 			$( "#naujas" ).button().on( "click", function() {
 				
-				$( '#name_item' ).html ( 'Naujas ' + i_am.title_dialog_form );
+				$( '#name_item' ).html ( 'Naujas ' + i_am.params.title_dialog_form );
 				
 			        $( '#id_prod' ).val(  '0' );
 				
-			        i_am.dialog.dialog( "open" );
+			        i_am.params.dialog.dialog( "open" );
 			});				
 			
 			$( '.edit_button' ).on ( 'click', function() {
@@ -115,36 +112,36 @@
 				
 				i_record = i_am.surastiSarasePagalId ( id_record );
 				
-				for ( k=0; k < i_am.fields.length; k++ ) {
+				for ( k=0; k < i_am.params.fields.length; k++ ) {
 					
-					field_val = eval (  'i_am.data' + i_am.field_data + '[ i_record ].' + i_am.fields [ k ] );
+					field_val = eval (  'i_am.params.data' + i_am.params.field_data + '[ i_record ].' + i_am.params.fields [ k ] );
 					
-																															// alert (  i_am.fields [ k ]  + ': ' + field_val );
-					$( '#' +  i_am.fields_ids [ k ] ).val ( field_val );
+																															// alert (  i_am.params.fields [ k ]  + ': ' + field_val );
+					$( '#' +  i_am.params.fields_ids [ k ] ).val ( field_val );
 				}
 				
-				$( '#name_item' ).html ( eval (  'i_am.data' + i_am.field_data + '[ i_record ].' + i_am.field_name ) );
+				$( '#name_item' ).html ( eval (  'i_am.params.data' + i_am.params.field_data + '[ i_record ].' + i_am.params.field_name ) );
 				
 				$( '#id_rec' ).val ( id_record );
-				i_am.dialog.dialog( "open" );				
+				i_am.params.dialog.dialog( "open" );				
 			});
 		}
 		 
 		this.sarasoAntraste= function() {
 			
-			this.res_str += '<tr><th>eil.<br>nr.</th>';
+			this.params.res_str += '<tr><th>eil.<br>nr.</th>';
 			
-			for ( i =0; i< this.fields_names.length; i++ ) {
+			for ( i =0; i< this.params.fields_names.length; i++ ) {
 			
-				this.res_str += '<th>' + this.fields_names [ i ] + '</th>';
+				this.params.res_str += '<th>' + this.params.fields_names [ i ] + '</th>';
 				
 			}
-			this.res_str += 
+			this.params.res_str += 
 			
 				'<th>veiksmai ' +									
 					'<input type="button" class="new_button" value="naujas" id="naujas"></th>' +
 				'</tr>';
-																													// alert ( this.res_str );
+																													// alert ( this.params.res_str );
 		}
 		
 		this.surastiSarasePagalId = function ( id ) {
@@ -153,7 +150,7 @@
 	
 			for ( i = 0; i < num_produktu; i++) {	
 			
-				if ( id == eval ( 'this.data' +this.field_data + '[ i ].id' ) ) {
+				if ( id == eval ( 'this.params.data' +this.params.field_data + '[ i ].id' ) ) {
 				
 					found = i;
 				}
@@ -163,9 +160,9 @@
 		
 		this.htmlDialogo = function() {
 			
-			this.res_str += 
+			this.params.res_str += 
 			
-				'<div id="dialogo_forma" title="' + this.title_dialog_form + '">' +
+				'<div id="dialogo_forma" title="' + this.params.title_dialog_form + '">' +
 					'<p class="validateTips">Visi laukeliai turi buti užpildyti.</p>' +
 					'<p class="name_item" id="name_item"></p>' +
 					'<form action="">' +
@@ -174,7 +171,7 @@
 
 			this.htmDialogoEditFields();
 
-			this.res_str += 			
+			this.params.res_str += 			
 			
 				'<!-- Allow form submission with keyboard without duplicating the dialog button -->' +
 				'<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">' +
@@ -183,21 +180,21 @@
 				'</form>' +
 				'</div>'
 			;
-																															// console.log ( this.res_str );
+																															// console.log ( this.params.res_str );
 		}
 		
 		this.htmDialogoEditFields = function() {
 		
-			for ( k=0; k < this.fields.length; k++ ) {
+			for ( k=0; k < this.params.fields.length; k++ ) {
 				
 				
-				if ( ( this.ed_fields.length > 0 ) && ( this.ed_fields.indexOf ( this.fields_ids [ k ] ) > -1 ) ) {
+				if ( ( this.params.ed_fields.length > 0 ) && ( this.params.ed_fields.indexOf ( this.params.fields_ids [ k ] ) > -1 ) ) {
 				
 				
-					this.res_str += 
+					this.params.res_str += 
 					
-						'<label for="' + this.fields_ids [ k ] + '">' + this.fields_names [ k ].replace( '-<br>', '' ).replace ( '<br>', ' ' ).replace( '_', '' ) + '</label>' +
-							'<input type="text" name="' + this.fields_ids [ k ] + '" id="' + this.fields_ids [ k ] + '" value="" class="text ui-widget-content ui-corner-all">'
+						'<label for="' + this.params.fields_ids [ k ] + '">' + this.params.fields_names [ k ].replace( '-<br>', '' ).replace ( '<br>', ' ' ).replace( '_', '' ) + '</label>' +
+							'<input type="text" name="' + this.params.fields_ids [ k ] + '" id="' + this.params.fields_ids [ k ] + '" value="" class="text ui-widget-content ui-corner-all">'
 					;
 				}
 			}		
@@ -207,7 +204,7 @@
 		this.saveRecord = function() {
 			
 			var valid = true;
-			i_am.allFields.removeClass( "ui-state-error" );
+			i_am.params.allFields.removeClass( "ui-state-error" );
 			
 			valid = i_am.validate();
 		 
@@ -219,12 +216,12 @@
 		
 		this.edFields = function () {
 		
-			for ( k=0; k < this.fields.length; k++ ) {
+			for ( k=0; k < this.params.fields.length; k++ ) {
 				
-				if ( ( this.ed_fields.length > 0 ) && ( this.ed_fields.indexOf ( this.fields_ids [ k ] ) > -1 ) ) {
+				if ( ( this.params.ed_fields.length > 0 ) && ( this.params.ed_fields.indexOf ( this.params.fields_ids [ k ] ) > -1 ) ) {
 
-					this.o_ed_fields.push ( $ ( '#' + this.fields [ k ] ) );
-					this.allFields.add( this.o_ed_fields [ k ]  );
+					this.params.o_ed_fields.push ( $ ( '#' + this.params.fields [ k ] ) );
+					this.params.allFields.add( this.params.o_ed_fields [ k ]  );
 				}
 			}
 		}
@@ -233,27 +230,27 @@
 			
 			params_str = '?';
 			
-			if ( i_am.url_save_rec.indexOf ( '?' ) > -1 ) {
+			if ( i_am.params.url_save_rec.indexOf ( '?' ) > -1 ) {
 			
 				params_str = '&';
 			}			
 		
 			params_str += 'id=' + id;  
 			
-			for ( k=0; k < i_am.ed_fields.length; k++ ) {
+			for ( k=0; k < i_am.params.ed_fields.length; k++ ) {
 				
-				params_str += '&' + i_am.ed_fields [ k ] + '=' + $( '#' + i_am.ed_fields [ k ] ).val();
+				params_str += '&' + i_am.params.ed_fields [ k ] + '=' + $( '#' + i_am.params.ed_fields [ k ] ).val();
 			}
 				
 			$.ajax(
 				{
-					url: i_am.url_save_rec + params_str
+					url: i_am.params.url_save_rec + params_str
 				}
 			)
 			.done( function( data ) {
 				
 																																	// alert ( data );
-				i_am.dialog.dialog ( 'close' );
+				i_am.params.dialog.dialog ( 'close' );
 				i_am.refreshData(); 																											// paimtiProduktus();
 			});
 		}		
@@ -266,13 +263,13 @@
 		
 		this.initDialog = function() {
 			
-			alert ( this.id_html_dialog_formos  );
+			alert ( this.params.id_html_dialog_formos  );
 			
-			i_am.dialog = $( '#' + this.id_html_dialog_formos ).dialog({
+			this.params.dialog = $( '#' + this.params.id_html_dialog_formos ).dialog({
 				
 				autoOpen: false
-				, height: this.dialog_size_y
-				, width: this.dialog_size_x
+				, height: this.params.dialog_size_y
+				, width: this.params.dialog_size_x
 				, modal: true
 				, buttons: {
 					
@@ -280,16 +277,16 @@
 					
 					, "Atšaukti": function() {
 				
-						i_am.dialog.dialog( "close" );
+						i_am.params.dialog.dialog( "close" );
 					}
 			      }
 			      , close: function() {
 				      
-					i_am.form[ 0 ].reset();
-					i_am.allFields.removeClass( "ui-state-error" );
+					i_am.params.form[ 0 ].reset();
+					i_am.params.allFields.removeClass( "ui-state-error" );
 			      }
 			});
 			 
-			this.form = this.dialog.find( 'form' );
+			this.params.form = this.params.dialog.find( 'form' );
 		}
 	}
